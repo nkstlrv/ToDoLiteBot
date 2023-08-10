87b466e1d74b1b7a804e36caedc2eb4cf56c199b
nkstlrv <stlrvnk@gmail.com>
2023-08-10 14:52:39 +0300

    some changes

--- bot/markups.py
+++ bot/markups.py
@@ -2,12 +2,10 @@ from aiogram import types
 
 
 class MainMenuMarkup:
-    b1 = types.InlineKeyboardButton("Add Task", callback_data="task_add")
-    b2 = types.InlineKeyboardButton("My ToDos", callback_data="tasks_show")
-    b3 = types.InlineKeyboardButton("Completed tasks", callback_data="tasks_show_done")
-    # b4 = types.InlineKeyboardButton("Mark as done", callback_data="tasks_mark_as_done")
-    # b5 = types.InlineKeyboardButton("Delete task", callback_data="tasks_delete")
-    markup = types.InlineKeyboardMarkup(row_width=1).add(b1, b2, b3)
+    b1 = types.KeyboardButton("/add_task")
+    b2 = types.KeyboardButton("/show_tasks")
+    b3 = types.KeyboardButton("/show_completed_tasks")
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True).add(b1, b2, b3)
 
 
 abort_btn = types.KeyboardButton("Abort")
